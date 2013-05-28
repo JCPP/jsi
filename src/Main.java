965c600884a9acd62a993e0967249db590a597d7
Matteo <emmeci92@gmail.com>
2013-05-28 19:33:29 +0200

    General project to Java Project.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author <a href="">Wanted.92</a>
+ *
+ */
+public class Main {
+	
+	/**
+	 * Here the application starts.
+	 * @param args
+	 */
+	public static void main(String args[]){
+		System.out.println("It works");
+	}
+
+}
